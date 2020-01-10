0ee40f0c51db9cbee1416bca659f8fa21e941ca2
susanspecs <ayomideosho13@gmail.com>
2020-01-10 12:36:56 +0100

    Updates

---
+++ precache-manifest.e6635e1b2bea3e80ccb6d88143c20137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b384eeca7d8e0410793da284f5feba",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "95fa1878034f70016591",
+    "url": "/first-react-project/static/css/main.6ce135c6.chunk.css"
+  },
+  {
+    "revision": "13411b9dcd351fdb79cf",
+    "url": "/first-react-project/static/js/2.3161583e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/first-react-project/static/js/2.3161583e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95fa1878034f70016591",
+    "url": "/first-react-project/static/js/main.20d1d2e6.chunk.js"
+  },
+  {
+    "revision": "7bbf2a7852f090aa2a7c",
+    "url": "/first-react-project/static/js/runtime-main.7e53a608.js"
+  }
+]);
